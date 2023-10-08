be58a6d138104e9f4d0451675b341dddb8bd29ad
mwerah <mohamed.werah@gmail.com>
2023-10-08 14:50:54 +0100

    sunday 2pm 08/10/23

---
+++ loops.js
@@ -0,0 +1,12 @@
+//write a for loop that prints your name 10 times without using codesnippets
+
+// for (let i=0; i<10; i++) {
+//     console.log("Mohamed")
+// }
+
+//write a while loop that prints your name 5 times
+let i =0;
+while (i<5) {
+    console.log("Mohamed")
+    i++;
+}
