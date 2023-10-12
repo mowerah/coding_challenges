d6b2f550e3164e0ea48a6740971aef1b6d39fc15
mwerah <mohamed.werah@gmail.com>
2023-10-12 20:27:44 +0100

    convert if else chain to a switch

--- js_coding_challenge.js
+++ js_coding_challenge.js
@@ -15,10 +15,49 @@
 
 //while loop that prints your surname 15 times
 
-// let i = 0;
-// while(i <15) {
-//     console.log("Mohamed");
+// write a do...while that prints your surname 100 times
+// var i = 0;
+// do {
+//     console.log("Mohamed...");
 //     i++;
+// }
+// while(i<2)
+
+// write an infinite loop using a while loop
+// while (true) {
+//     console.log("hello...")
+// }
+
+// use if/else if chain to check the day of the week and print "it is work time" for monday, tuesday "it is school time", for friday "it is off day"
+// let day = "Wednesday"
+// if (day == "Monday") {
+//     console.log("it is work time")
+    
+// } else if (day == "Tuesday") {
+//     console.log("it is School time")    
+// } 
+// else {
+//     console.log("it is off time")
+// }
+
+// convert the if/else if to switch statement
+
+switch (day="Monday") {
+    case "Monday":
+        console.log("it is work time.. switch")
+        break;
+    case "Tuesday":
+        console.log("it is work time.. switch")
+        break;
+
+    default:
+        console.log("it is off time.. switch")
+        break;
+}
+
+//write an infinite for loop
+// for (;;) {
+//     console.log("hello..")
 // }
 
 //write array of 10 elements of string(for example 10 students)
@@ -37,7 +76,7 @@
 //students.filter()
 //students.reduce()
 
-//implement forEach function that prints all the elements of the array bbelow
+//implement forEach function that prints all the elements of the array below
 // students.forEach(
 //     function(value, index) {
 //         console.log("the lement is " + value + " and its index is " + index);
@@ -55,5 +94,5 @@
 // }
 // console.log(multiply(5,3))
 
-let multiply = (num1, num2) => num1 * num2;
-console.log(multiply(5,3))
+// let multiply = (num1, num2) => num1 * num2;
+// console.log(multiply(5,3))
